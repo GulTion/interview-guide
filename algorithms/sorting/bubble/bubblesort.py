0fe7cd94a072bb89edaa86455931d75645227ba4
GulTion <44129143+GulTion@users.noreply.github.com>
2020-04-07 17:31:36 +0530

    bubblesort.py
    Here We can swap to element like this

--- algorithms/sorting/bubble/bubblesort.py
+++ algorithms/sorting/bubble/bubblesort.py
@@ -1,9 +1,8 @@
+def swap(a,b):
+    return b, a
 def bubblesort(array):
     for i in range(0, len(array)):
         for j in range(0, len(array)):
             if array[j] > array[i]:
-                array[i] += array[j]
-                array[j] = array[i] - array[j]
-                array[i] -= array[j]
-
+                array[i], array[j] = swap(array[i], array[j])
     return array
